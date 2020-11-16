65cdecf34edce8b475c581f8532589b2f10b793b
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-11-16 11:19:29 -0500

    Rails API for songs complete, .yml updated, gems installed, cors allowed, db created, db migration, seeded

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create([
+    {title: "Fast Car", artist: "Tracy Chapman", time: "3:55"},
+    {title: "Sunset Lover", artist: "Petit Biscuit", time: "3:45"},
+    {title: "Glory Days", artist: "Bruce Springsteen", time: "4:00"}
+])
